50ea9d4cb1f03b0b4c9baea58bc886684e432298
Calum-W <cal7907@hotmail.co.uk>
2018-01-30 12:29:04 +0000

    Test items other than Sulfuras, BSP and Aged Brie"

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -59,18 +59,94 @@ describe GildedRose do
       end
     end
 
-    context "Backstage passes" do
+    context "Backstage passes (BSP)" do
       context "Quality" do
+        it "increase quality by two if sell_in is between 6-10" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 12
+        end
 
+        it "increase quality by three if sell_in is between 1-5" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 10)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 13
+        end
+
+        it "sets quality to 0 if sell_in is 0 or below" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 0
+        end
+
+        it "doesn't increase the value above 50 when sell_in is above 10" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 50)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 50
+        end
+
+        it "doesn't increase the value above 50 when sell_in is between 6-10" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 8, 50)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 50
+        end
+
+        it "doesn't increase the value above 50 when sell_in is between 1-5" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 50)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 50
+        end
       end
 
-      context "sell_in" do
-
+      context "Sell_in" do
+        it "reduces BSP sell_in by one" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 50)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq 9
+        end
       end
     end
 
     context "Other item" do
+      context "Quality" do
+        it "reduces the quality by one if sell_in is above 0" do
+          items = [Item.new("Charles's hat", 1, 10)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 9
+        end
 
+        it "reduces the quality by two if sell_in is 0 or below" do
+          items = [Item.new("Charles's hat", 0, 10)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 8
+        end
+
+        it "doesn't reduce the quality if already at 0" do
+          items = [Item.new("Charles's hat", 5, 0)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 0
+        end
+
+        it "doesn't reduce the quality if already at 0 and sell_in is 0 or below" do
+          items = [Item.new("Charles's hat", 0, 0)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 0
+        end
+
+        it "reduces the quality to 0 if currently at 1 and sell_in is 0 or below" do
+          items = [Item.new("Charles's hat", 0, 1)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 0
+        end
+      end
+
+      context "Sell_in" do
+        it "reduces sell_in by one" do
+          items = [Item.new("Joe's duck", 10, 50)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].sell_in).to eq 9
+        end
+      end
     end
   end
 end
