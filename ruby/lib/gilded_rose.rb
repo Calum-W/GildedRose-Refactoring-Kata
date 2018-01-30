943f199f8890be28ac636469eb36354769e92daf
Calum-W <cal7907@hotmail.co.uk>
2018-01-30 11:38:30 +0000

    Add lib and spec files. Comment on logic tree

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -9,12 +9,16 @@ class GildedRose
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
+            # If not Brie, BSP or Sulfuras, lose 1 quality (min 0)
             item.quality = item.quality - 1
           end
         end
       else
         if item.quality < 50
+          # Brie, BSP and Sulfuras gain 1 quality (max 50)
           item.quality = item.quality + 1
+
+          # BSP ains 1 or 2 quality if sell_in is under 11 or 6 (max 50)
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
@@ -29,21 +33,28 @@ class GildedRose
           end
         end
       end
+
+      # non-magical items lose 1 sell_in
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
+
+      # Sell-in < 0 only below here
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
+            # Sell-in < 0. If not Brie, BSP or Sulfuras, lose 1 quality
               if item.name != "Sulfuras, Hand of Ragnaros"
                 item.quality = item.quality - 1
               end
             end
           else
+            # Sell-in < 0. Backstage pass sets quality to 0
             item.quality = item.quality - item.quality
           end
         else
+          # Sell-in < 0. Aged Brie gains 1 quality (max 50)
           if item.quality < 50
             item.quality = item.quality + 1
           end
